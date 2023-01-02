cf01641d632eb8f1242df728b070e85fc6fc9f25
Rishabh <rishabh1428.be20@chitkara.edu.in>
2023-01-03 00:28:42 +0530

    created react app and build structure of sidebar and chat component

---
+++ src/Chat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Chat.css";
+function Chat() {
+  return (
+    <div className='chat'>
+        <h1>i am the chat component</h1>
+    </div>
+  )
+}
+
+export default Chat
