7964be950be8321e4b3b7433c5f5f9dfd152e5b3
Rishabh <rishabh1428.be20@chitkara.edu.in>
2023-02-21 23:34:28 +0530

    added pusher for realtime database

--- messenger-app-frontend/src/App.js
+++ messenger-app-frontend/src/App.js
@@ -1,8 +1,36 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
+import Pusher from 'pusher-js';
+import axios from "./axios";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  useEffect(()=>{
+    axios.get('/messages/sync')
+      .then((response) => {
+        setMessages(response.data);
+      })
+      .catch(err => console.log(err))
+  }, []);
+
+  useEffect(()=>{
+    const pusher = new Pusher('cf58539a60a53ccff600', {
+      cluster: 'ap2',
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (data)=> {
+      alert(JSON.stringify(data));
+    });
+
+    
+
+  }, []);
+
+  console.log(messages);
+
   return (
     <div className="app">
       <div className='app-body'>
